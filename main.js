770020270fad29f17e04809f404000fedf47bc64
pexemo <67392584+pexemo@users.noreply.github.com>
2021-06-03 21:25:30 +0430

    Main part completed!
    - some style changes
    - added Equal to cases

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ const winLine = document.querySelector('#winLine');
 const winDow = document.querySelector('#winDow');
 // who's turn is
 let turnX = true;
-let whoWin;
 // X moves
 let xPeac = [];
 // O moves
@@ -21,34 +20,37 @@ function getElIndex(el) {
 
 // check who won
 function win(array) {
-    if (array.includes(2) && array.includes(4) && array.includes(6)) {
-        winLine.style.transform = "translate(-50%, -50%) rotate(45deg)"
+    if (array.includes(0) && array.includes(4) && array.includes(8)) {
+        winLine.style.transform = "translate(-50%, -50%) rotate(-45deg)";
+        return true;
+    } else if (array.includes(2) && array.includes(4) && array.includes(6)) {
+        winLine.style.transform = "translate(-50%, -50%) rotate(45deg)";
         return true;
     } else if (array.includes(0) && array.includes(1) && array.includes(2)) {
-        winLine.style.transform = "translate(-50%, -50%) rotate(90deg)"
-        winLine.style.top = "16.5%";
+        winLine.style.transform = "translate(-50%, -50%) rotate(90deg)";
+        winLine.style.top = "16.18%";
         return true;
     } else if (array.includes(3) && array.includes(4) && array.includes(5)) {
-        winLine.style.transform = "translate(-50%, -50%) rotate(90deg)"
+        winLine.style.transform = "translate(-50%, -50%) rotate(90deg)";
         return true;
     } else if (array.includes(6) && array.includes(7) && array.includes(8)) {
-        winLine.style.transform = "translate(-50%, -50%) rotate(90deg)"
-        winLine.style.top = "82.5%";
+        winLine.style.transform = "translate(-50%, -50%) rotate(90deg)";
+        winLine.style.top = "82.79%";
         return true;
     } else if (array.includes(0) && array.includes(3) && array.includes(6)) {
-        winLine.style.transform = "translate(-50%, -50%)"
-        winLine.style.left = "16.5%";
+        winLine.style.transform = "translate(-50%, -50%)";
+        winLine.style.left = "16.18%";
         return true;
     } else if (array.includes(1) && array.includes(4) && array.includes(7)) {
-        winLine.style.transform = "translate(-50%, -50%)"
+        winLine.style.transform = "translate(-50%, -50%)";
         return true;
     } else if (array.includes(2) && array.includes(5) && array.includes(8)) {
-        winLine.style.transform = "translate(-50%, -50%)"
-        winLine.style.left = "82.5%";
+        winLine.style.transform = "translate(-50%, -50%)";
+        winLine.style.left = "82.79%";
         return true;
-    } else if (array.includes(0) && array.includes(4) && array.includes(8)) {
-        winLine.style.transform = "translate(-50%, -50%) rotate(-45deg)"
-        return true;
+    } else if (array.length >= 5) {
+        winDowOpen("equal");
+        return false;
     }
     winLine.style.height = getComputedStyle(container).height;
 }
@@ -56,6 +58,7 @@ function win(array) {
 function winDowClose() {
     setTimeout(() => {
         winDow.style.display = "none";
+        winLine.style.display = "none";
     }, 500);
     winDow.style.opacity = "0";
     setTimeout(() => {
@@ -63,18 +66,23 @@ function winDowClose() {
     }, 1000);
 }
 
-function winDowOpen() {
+function winDowOpen(whoWin) {
+    winDow.style.display = "block";
     setTimeout(() => {
         winDow.style.opacity = "1";
     }, 1);
-    winDow.style.display = "block";
-    winLine.style.display = "block";
-    winDow.querySelector('h1').innerText = whoWin + " Win!";
+    if (whoWin === "equal") {
+        winDow.querySelector('h1').innerText = "Equal!";
+    } else {
+        console.log(getComputedStyle(winLine).transform);
+        winLine.style.display = "block";
+        winDow.querySelector('h1').innerText = whoWin + " Win!";
+    }
 }
 
-const Xpot = '<svg version="1.1" id="prefix__Layer_1" xmlns="http://www.w3.org/2000/svg" x="0" y="0" viewBox="0 0 512 512" xml:space="preserve"><style>.prefix__st0{fill:none;stroke:orange;stroke-width:100;stroke-linecap:round;stroke-miterlimit:10}</style><path class="prefix__st0" d="M116 116l280 280M116 396l280-280"/></svg>';
+const Xpot = '<svg version="1.1" id="prefix__Layer_1" xmlns="http://www.w3.org/2000/svg" x="0" y="0" viewBox="0 0 512 512" xml:space="preserve" style="height: 100%; width: 100%;"><style>.prefix__st0{fill:none;stroke:orange;stroke-width:100;stroke-linecap:round;stroke-miterlimit:10}</style><path class="prefix__st0" d="M116 116l280 280M116 396l280-280"/></svg>';
 
-const Opot = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><circle cx="256" cy="256" r="180.4" fill="none" stroke="orange" stroke-width="75" stroke-linecap="round" stroke-miterlimit="10"/></svg>';
+const Opot = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="height: 100%; width: 100%;"><circle cx="256" cy="256" r="180.4" fill="none" stroke="orange" stroke-width="75" stroke-linecap="round" stroke-miterlimit="10"/></svg>';
 
 function move(e) {
     if (turnX === false) {
@@ -85,8 +93,7 @@ function move(e) {
         // add element index of parent
         oPeac.push(getElIndex(e));
         if (win(oPeac)) {
-            whoWin = "O";
-            winDowOpen();
+            winDowOpen("O");
         }
     } else {
         // It's turn X
@@ -96,8 +103,7 @@ function move(e) {
         // add element index of parent
         xPeac.push(getElIndex(e));
         if (win(xPeac)) {
-            whoWin = "X";
-            winDowOpen();
+            winDowOpen("X");
         }
     }
 }
